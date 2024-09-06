408dc43dfde6cabab6565098a563220234daaf3c
pinder99 <52063297+pushpinder6805@users.noreply.github.com>
2024-09-06 20:22:56 +0530

    2nd page

---
+++ javascripts/discourse/components/custom-homepage-content1.js
@@ -0,0 +1,14 @@
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
+
+export default class CustomHomepageContent extends Component {
+  @service router;
+
+  get isCustomHomepage() {
+    const { currentURL } = this.router;
+    const customPath = 'c/free-educational-videos/20/none';
+    console.log("Current URL:", currentURL);  // Debugging: Check the URL
+    return currentURL === customPath;
+  }
+}
+
