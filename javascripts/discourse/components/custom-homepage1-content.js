5dac08746a1e26c4827f43acb9b2b61fbc32b839
Pushpender Singh <52063297+pushpinder6805@users.noreply.github.com>
2024-09-07 12:29:58 +0530

    Update custom-homepage1-content.js

--- javascripts/discourse/components/custom-homepage1-content.js
+++ javascripts/discourse/components/custom-homepage1-content.js
@@ -1,10 +1,10 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
 
-export default class CustomHomepage1Content extends Component {
+export default class CustomHomepageContent extends Component {
   @service router;
 
-  get isCustomHomepage1() {
+  get isCustomHomepage() {
     const { currentURL } = this.router;
     const customPath = 'c/free-educational-videos/none';
     console.log("Current URL:", currentURL);  // Debugging: Check the URL
