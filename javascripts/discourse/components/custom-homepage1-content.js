8783ea8338186f818296779b30f4ecbe39844a69
pinder99 <52063297+pushpinder6805@users.noreply.github.com>
2024-09-07 12:46:04 +0530

    Update custom-homepage1-content.js

--- javascripts/discourse/components/custom-homepage1-content.js
+++ javascripts/discourse/components/custom-homepage1-content.js
@@ -1,3 +1,4 @@
+
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
 
@@ -6,7 +7,7 @@ export default class CustomHomepageContent extends Component {
 
   get isCustomHomepage() {
     const { currentURL } = this.router;
-    const customPath = 'c/free-educational-videos/none';
+    const customPath = '/c/free-educational-videos/20/none';
     console.log("Current URL:", currentURL);  // Debugging: Check the URL
     return currentURL === customPath;
   }
