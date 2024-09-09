8293e0ee40380c08aefb9e98138dda622360508c
pinder99 <52063297+pushpinder6805@users.noreply.github.com>
2024-09-10 03:59:14 +0530

    Update custom-homepage5-content.js

--- javascripts/discourse/components/custom-homepage5-content.js
+++ javascripts/discourse/components/custom-homepage5-content.js
@@ -6,7 +6,7 @@ export default class CustomHomepageContent extends Component {
 
   get isCustomHomepage() {
     const { currentURL } = this.router;
-    const customPaths = ['/c/free-educational-videos/angel-investing-basics/38', '/c/free-educational-videos/angel-investing-basics/38'];
+    const customPaths = ['/c/free-educational-videos/sustainable-agriculture-investing/42', '/c/free-educational-videos/sustainable-agriculture-investing/42'];
     
     console.log("Current URL:", currentURL);  // Debugging: Check the URL
     return customPaths.includes(currentURL);
