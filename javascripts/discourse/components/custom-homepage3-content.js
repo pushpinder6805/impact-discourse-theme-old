984eea9fef355f4d935dfffd3f2d29be2150fd5b
pinder99 <52063297+pushpinder6805@users.noreply.github.com>
2024-09-10 01:11:51 +0530

    Update custom-homepage3-content.js

--- javascripts/discourse/components/custom-homepage3-content.js
+++ javascripts/discourse/components/custom-homepage3-content.js
@@ -6,9 +6,8 @@ export default class CustomHomepageContent extends Component {
 
   get isCustomHomepage() {
     const { currentURL } = this.router;
-    const customPaths = ['/c/private-opportunities/23'];
-    
+    const customPath = '/c/private-opportunities/23';
     console.log("Current URL:", currentURL);  // Debugging: Check the URL
-    return customPaths.includes(currentURL);
+    return currentURL === customPath;
   }
 }
