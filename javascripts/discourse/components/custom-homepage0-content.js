0914fb6d68468349d391e7840807e14933a373bc
pinder99 <pinder.chaudhary@gmail.com>
2024-11-22 13:09:55 +0530

    Update custom-homepage0-content.js

--- javascripts/discourse/components/custom-homepage0-content.js
+++ javascripts/discourse/components/custom-homepage0-content.js
@@ -6,7 +6,7 @@ export default class CustomHomepageContent extends Component {
 
   get isCustomHomepage() {
     const { currentURL } = this.router;
-    const customPaths = ['/c/private-opportunities/cal/68', '/c/private-opportunities/cal/68'];
+    const customPaths = ['/c/private-opportunities/phase2-dashboard/68', '/c/private-opportunities/phase2-dashboard/68'];
     
     console.log("Current URL:", currentURL);  // Debugging: Check the URL
     return customPaths.includes(currentURL);
