22a31d4bb8d5712b3b2b6b538cc830c243ebb0a4
pinder99 <pinder.chaudhary@gmail.com>
2024-12-18 23:51:04 +0530

    Update custom-homepage01-content.js

--- javascripts/discourse/components/custom-homepage01-content.js
+++ javascripts/discourse/components/custom-homepage01-content.js
@@ -6,7 +6,7 @@ export default class CustomHomepageContent extends Component {
 
   get isCustomHomepage() {
     const { currentURL } = this.router;
-    const customPaths = ['/c/private-opportunities/mastery-series/70', 'c/private-opportunities/mastery-series/70'];
+    const customPaths = ['https://impactinvestingcommunity.com/login', 'https://impactinvestingcommunity.com/login'];
     
     console.log("Current URL:", currentURL);  // Debugging: Check the URL
     return customPaths.includes(currentURL);
