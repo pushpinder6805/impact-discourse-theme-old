ddbd62651953ed1f5f18b6050cab6523f5a80a4e
pinder99 <pinder.chaudhary@gmail.com>
2024-12-19 15:01:21 +0530

    login page check

--- javascripts/discourse/components/custom-homepage123-content.js
+++ javascripts/discourse/components/custom-homepage123-content.js
@@ -6,9 +6,9 @@ export default class CustomHomepageContent extends Component {
 
   get isCustomHomepage() {
     const { currentURL } = this.router;
-    const customPaths = ['https://impactinvestingcommunity.com/login', 'https://impactinvestingcommunity.com/login'];
+    const customPaths = ['/login', '/login'];
     
-    console.log("Current URL:", currentURL);  // Debugging: Check the URL
+    console.log("Current URL:", currentURL);  // Debugging: Check the URL 
     return customPaths.includes(currentURL);
   }
 }
